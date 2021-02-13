2075ffc16a7131a6ac5f138dd4c47d9a9ef2d643
ac4574 <50218377+ac4574@users.noreply.github.com>
2021-02-13 13:06:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+
+async function quickstart() {
+  const filename = /*path to audio file here. avoid mp4 files*/ ;
+  const encoding = 'LINEAR16';
+  const sampleRateHertz = 16000;
+  const languageCode = 'en-US';
+
+  const config = {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  };
+
+  /**
+   * Note that transcription is limited to 60 seconds audio.
+   * Use a GCS file for audio longer than 1 minute.
+   */
+  const audio = {
+    content: fs.readFileSync(filename).toString('base64'),
+  };
+
+  const request = {
+    config: config,
+    audio: audio,
+  };
+
+  // Detects speech in the audio file. This creates a recognition job that you
+  // can wait for now, or get its result later.
+  const [operation] = await client.longRunningRecognize(request);
+
+  // Get a Promise representation of the final result of the job
+  const [response] = await operation.promise();
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+
+}
+quickstart()
