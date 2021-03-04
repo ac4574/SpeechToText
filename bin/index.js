dc6dfefd9d867b26880e423cc6553753cacf245c
Anderson Chan <andersonchan4574@gmail.com>
2021-03-04 15:28:11 -0500

    started process of publishing package

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const {speechToFreqChart} = require('../index')
+
+speechToFreqChart(process.argv[2])
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Imports the Google Cloud client library
-const { projectId, keyFilename } = require('./keys/secrets')
+const { projectId, keyFilename, audioFilePath, bucketName } = require('./keys/secrets')
 const speech = require('@google-cloud/speech');
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage({projectId, keyFilename});
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({ projectId, keyFilename });
 const sound_files = storage.bucket('sound_files_gaa');
 
 // Creates a client
 const client = new speech.SpeechClient();
 
-async function quickstart() {
+async function speechToFreqChart(filename) {
 
   // if(process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   //   console.log('It is set!');
@@ -17,15 +17,15 @@ async function quickstart() {
   //     console.log('Not set!');
   // }
 
-  const filename = 'test4.flac'
+  const filename = 'test4'
 
   const options = {
-    destination: filename,
+    destination: `${filename}.flac`,
     public:true
   }
-  await sound_files.upload(`/Users/andersonchan/SpeechToText/audio-files/${filename}`, options)
+  await sound_files.upload(`${audioFilePath}${filename}.flac`, options)
 
-  const gcsUri = `gs://sound_files_gaa/${filename}`;
+  const gcsUri = `${bucketName}${filename}.flac`;
   const encoding = 'FLAC';
   const sampleRateHertz = 16000;
   const languageCode = 'en-US';
@@ -66,4 +66,8 @@ async function quickstart() {
 
   console.log(`Chart: ${JSON.stringify(dict)}`)
 }
-quickstart()
+speechToFreqChart()
+
+module.exports = {
+  speechToFreqChart
+}
