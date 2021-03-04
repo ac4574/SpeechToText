e705709577e31a76ece0a3fed6a20222445f77b7
Anderson Chan <andersonchan4574@gmail.com>
2021-03-04 14:51:28 -0500

    created secrets folder and modularized the filename usage

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Imports the Google Cloud client library
+const { projectId, keyFilename } = require('./keys/secrets')
 const speech = require('@google-cloud/speech');
 const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
+const storage = new Storage({projectId, keyFilename});
 const sound_files = storage.bucket('sound_files_gaa');
 
 // Creates a client
@@ -9,30 +10,30 @@ const client = new speech.SpeechClient();
 
 async function quickstart() {
 
-  if(process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    console.log('It is set!');
-  }
-  else {
-      console.log('Not set!');
-  }
+  // if(process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  //   console.log('It is set!');
+  // }
+  // else {
+  //     console.log('Not set!');
+  // }
 
+  const filename = 'test4.flac'
 
   const options = {
-    destination: 'uploadtest.flac',
+    destination: filename,
     public:true
   }
-  await sound_files.upload('/Users/andersonchan/SpeechToText/audio-files/uploadtest.flac', options)
+  await sound_files.upload(`/Users/andersonchan/SpeechToText/audio-files/${filename}`, options)
 
-  const gcsUri = 'gs://sound_files_gaa/Untitled.flac';
+  const gcsUri = `gs://sound_files_gaa/${filename}`;
   const encoding = 'FLAC';
-  const sampleRateHertz = 48000;
+  const sampleRateHertz = 16000;
   const languageCode = 'en-US';
 
   const config = {
     encoding: encoding,
     sampleRateHertz: sampleRateHertz,
     languageCode: languageCode,
-    audioChannelCount: 2
   };
 
   const audio = {
@@ -50,7 +51,7 @@ async function quickstart() {
   // Get a Promise representation of the final result of the job
   const [response] = await operation.promise();
   const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
+    .map(result => (result.alternatives[0].transcript).trim())
 
   // Create a frequency chart called 'dict' with the transcript
   const dict = {}
